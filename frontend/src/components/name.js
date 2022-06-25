715bacd87450945b03e41ec3aa1fd6fe8fea7acd
sumittapuriso <kingta7215@gmail.com>
2022-06-25 15:41:37 +0700

    first commit

---
+++ frontend/src/components/name.js
@@ -0,0 +1,11 @@
+function MyName() {
+    return (
+    <div className="MyName">
+    <div>นางสาวสุมิตตา ปุริโส
+    </div>
+    <div>610112418051</div>
+    </div>
+
+    );
+}
+export default MyName;
